f56413562a6aff5e427f49f1e9bcfc5bc1d90117
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2019-07-29 08:44:12 +0100

    Added side nav

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,16 @@
-import '../stylesheets/home.scss';
+const toggle = document.querySelector('#toggle');
+
+const fixedNavbar = document.querySelector('#navbar-fixed');
+const topNav = document.querySelector('#topnav');
+const bottomNav = document.querySelector('#bottomNav');
+const showSideNav = () => {
+  fixedNavbar.style.width = '220px';
+  topNav.style.display = 'flex';
+  bottomNav.style.display = 'flex'; 
+}
+
+const hideSideNav = () => {
+  fixedNavbar.style.width = '0px';
+  topNav.style.display = 'none';
+  bottomNav.style.display = 'none'; 
+}
