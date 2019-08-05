8724d0d3a94fac083da01c8772d29c23590daf2e
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2019-08-05 11:18:13 +0100

    Added dark mode

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,9 +4,15 @@ const fixedNavbar = document.querySelector('#navbar-fixed');
 const topNav = document.querySelector('#topnav');
 const bottomNav = document.querySelector('#bottomNav');
 const showSideNav = () => {
-  fixedNavbar.style.width = '220px';
-  topNav.style.display = 'flex';
-  bottomNav.style.display = 'flex'; 
+  if (fixedNavbar.style.width == "0px") {
+    fixedNavbar.style.width = '220px';
+    topNav.style.display = 'flex';
+    bottomNav.style.display = 'flex'; 
+  } else {
+    fixedNavbar.style.width = '0px';
+    topNav.style.display = 'none';
+    bottomNav.style.display = 'none';
+  }
 }
 
 const hideSideNav = () => {
