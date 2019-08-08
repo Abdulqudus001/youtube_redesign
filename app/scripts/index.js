48bd4acadc020ac24686588454738e7841e8d8e7
Abdulqudus001 <abdulqudusabubakre@gmail.com>
2019-08-08 15:53:32 +0100

    Seperated navbar component

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,17 +1,21 @@
 const toggle = document.querySelector('#toggle');
-
 const fixedNavbar = document.querySelector('#navbar-fixed');
 const topNav = document.querySelector('#topnav');
 const bottomNav = document.querySelector('#bottomNav');
+const videos = document.querySelector('#videos');
+
 const showSideNav = () => {
   if (fixedNavbar.style.width == "0px") {
     fixedNavbar.style.width = '220px';
     topNav.style.display = 'flex';
-    bottomNav.style.display = 'flex'; 
+    bottomNav.style.display = 'flex';
+    videos.style.margin = '0 0 0 220px'; 
   } else {
     fixedNavbar.style.width = '0px';
     topNav.style.display = 'none';
     bottomNav.style.display = 'none';
+    videos.style.margin = '0 auto';
+
   }
 }
 
